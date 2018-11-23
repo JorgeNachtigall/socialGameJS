5cfb5fef32bba2bbb591818823c10abde2e2822f
JorgeNachtigall <jlnvjunior@gmail.com>
2018-11-23 17:55:54 -0200

    procfile fixed

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
 var socket = require('socket.io');
 var io = socket(server);
 app.use(express.static('public'));
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Server is running. Listening on PORT: " + port);
+});
 
 var connectedPlayers = {};
 
